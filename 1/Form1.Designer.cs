2e2ab8149c1262ce78123df000ec446369b791b6
Группа ИСП-31 <-31@rssk.local>
2020-10-27 10:31:38 +0300

    Добавьте файлы проекта.

---
+++ 1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.расчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сбросToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.numbers = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.work = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kolvo = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(391, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.расчитатьToolStripMenuItem,
+            this.сбросToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // расчитатьToolStripMenuItem
+            // 
+            this.расчитатьToolStripMenuItem.Name = "расчитатьToolStripMenuItem";
+            this.расчитатьToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.расчитатьToolStripMenuItem.Text = "Сгенерировать числа";
+            this.расчитатьToolStripMenuItem.Click += new System.EventHandler(this.work_Click);
+            // 
+            // сбросToolStripMenuItem
+            // 
+            this.сбросToolStripMenuItem.Name = "сбросToolStripMenuItem";
+            this.сбросToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.сбросToolStripMenuItem.Text = "Очистить";
+            this.сбросToolStripMenuItem.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // numbers
+            // 
+            this.numbers.Location = new System.Drawing.Point(12, 56);
+            this.numbers.Name = "numbers";
+            this.numbers.ReadOnly = true;
+            this.numbers.Size = new System.Drawing.Size(367, 26);
+            this.numbers.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Сгенерированные числа";
+            // 
+            // work
+            // 
+            this.work.Location = new System.Drawing.Point(16, 186);
+            this.work.Name = "work";
+            this.work.Size = new System.Drawing.Size(139, 51);
+            this.work.TabIndex = 5;
+            this.work.Text = "Сгенерировать числа";
+            this.work.UseVisualStyleBackColor = true;
+            this.work.Click += new System.EventHandler(this.work_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(204, 209);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(175, 28);
+            this.clear.TabIndex = 6;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Сумма";
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(12, 130);
+            this.sum.Name = "sum";
+            this.sum.ReadOnly = true;
+            this.sum.Size = new System.Drawing.Size(161, 26);
+            this.sum.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Кол-во чисел";
+            // 
+            // kolvo
+            // 
+            this.kolvo.Location = new System.Drawing.Point(204, 130);
+            this.kolvo.Name = "kolvo";
+            this.kolvo.Size = new System.Drawing.Size(113, 26);
+            this.kolvo.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 249);
+            this.Controls.Add(this.kolvo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.work);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numbers);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Практическая работа 1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox numbers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button work;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox kolvo;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem расчитатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сбросToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+    }
+}
+
---
+++ 1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lib_1;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // расчет
+        private void work_Click(object sender, EventArgs e)
+        {
+            sum.Clear();
+            numbers.Clear();
+          
+            int k,
+                Sum;
+            
+            if (Int32.TryParse(kolvo.Text, out k)==true)
+            {
+                // вызов функции
+                Sum = Lib_1.RandomAmount.Go(k, numbers);
+                sum.Text = Sum.ToString();
+            }
+           
+            else
+            {
+                kolvo.Clear();
+                MessageBox.Show("Введите правильное значение");
+            }
+        }
+        //Сброс
+        private void reset_Click(object sender, EventArgs e)
+        {
+            sum.Clear();
+            numbers.Clear();
+            kolvo.Clear();
+        }
+        //Изменение входного текстбокса
+        private void sourceK_TextChanged(object sender, EventArgs e)
+        {
+            sum.Clear();
+            numbers.Clear();
+            kolvo.Clear();
+        }
+        //О программе
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Практическая работа № 1, 12 вариант\n" +
+                "Найти сумму n целых случайных чисел, распределенных в диапазоне от 0 до n. Вывести на экран на одной строке сгенерированные числа, на другой строке результат. \n" +
+                "Платов Андрей ИСП-31");
+        }
+        // выход
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
